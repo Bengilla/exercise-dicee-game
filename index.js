9c0e55b4e42c90fd8bd222d41f7ccaaf5f62d057
Bengilla <bengilla@outlook.com>
2022-01-15 11:40:14 +0800

    Initial commit

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+function game() {
+  var player1 = Math.floor(Math.random() * 6) + 1;
+  var player2 = Math.floor(Math.random() * 6) + 1;
 
-var player1 = Math.floor(Math.random() * 6) + 1;
-var player2 = Math.floor(Math.random() * 6) + 1;
+  document.querySelector('.img1').style.backgroundImage = 'url("img/dice' + player1 + '.png")'
+  document.querySelector('.img2').style.backgroundImage = 'url("img/dice' + player2 + '.png")'
 
-document.querySelector('.img1').style.backgroundImage = 'url("img/dice' + player1 + '.png")'
-document.querySelector('.img2').style.backgroundImage = 'url("img/dice' + player2 + '.png")'
-
-if (player1 === player2) {
-  document.querySelector('h1').innerHTML = 'Draw!';
-} else if (player1 > player2) {
-  document.querySelector('h1').innerHTML = 'Player 1 Win!!!';
-} else {
-  document.querySelector('h1').innerHTML = 'Player 2 Win!!!';
+  if (player1 === player2) {
+    document.querySelector('h1').innerHTML = 'Draw!';
+  } else if (player1 > player2) {
+    document.querySelector('h1').innerHTML = 'Player 1 Win!!!';
+  } else {
+    document.querySelector('h1').innerHTML = 'Player 2 Win!!!';
+  }
 }
